6be79f021e41760ff4a79658fd2b73a4e00955bc
SUP'Internet 05 <supinternet05@macbook-air-de-supinternet-7.local>
2018-10-25 14:23:03 +0200

    Initial Commit

---
+++ ListProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListProject
+//
+//  Created by SUP'Internet 05 on 25/10/2018.
+//  Copyright © 2018 SUP'Internet 05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
